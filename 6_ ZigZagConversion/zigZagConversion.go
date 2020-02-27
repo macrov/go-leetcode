f8fc75dfcec073d44c3b2dcd24cf34387cb430c9
Macrov Gao <backfire.ghw@gmail.com>
2020-02-27 10:59:06 +0800

    update zigZagConversion

--- 6_ ZigZagConversion/zigZagConversion.go
+++ 6_ ZigZagConversion/zigZagConversion.go
@@ -1,31 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func convert(s string, numRows int) string {
 	if numRows < 2 {
 		return s
 	}
-	matrix := make([]string, numRows)
-	dir := 1
+	matrix := make([]bytes.Buffer, numRows)
+	direction := 1 // 1 down, -1 up
 	current_row := 0
 	for i, c := range s {
-		matrix[current_row] = matrix[current_row] + string(c)
+		// append char
+		matrix[current_row].WriteRune(c)
+		// change direction
 		if (current_row == 0 && i != 0) || current_row == numRows -1 {
-			dir = -dir
+			direction = -direction
 		}
-		if dir > 0 {
+		// change pointer by direction
+		if direction > 0 {
 			current_row++
 		} else {
 			current_row--
 		}
 	}
-	res := ""
-	for _, str := range matrix {
-		res = res + str
+	res := bytes.Buffer{}
+	// merge all strings
+	for _, buffer := range matrix {
+		res.Write(buffer.Bytes())
 	}
-	return res
+	return res.String()
 }
+
+//func convert(s string, numRows int) string {
+//	if numRows < 2 {
+//		return s
+//	}
+//	matrix := make([]bytes.Buffer, numRows)
+//	for i, c := range s {
+//		indexByZone := i % (numRows*2 - 2)
+//		if indexByZone < numRows {
+//			matrix[indexByZone].WriteRune(c)
+//		} else {
+//			matrixIndex := numRows - indexByZone%numRows - 2
+//			matrix[matrixIndex].WriteRune(c)
+//		}
+//	}
+//	res := bytes.Buffer{}
+//	for _, buffer := range matrix {
+//		res.WriteString(buffer.String())
+//	}
+//	return res.String()
+//}
 
 func main() {
 	fmt.Println(convert("PAYPALISHIRING", 3))
