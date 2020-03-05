3abd684e6ff89ef907ae28d5bfd3868823bd985f
Macrov Gao <backfire.ghw@gmail.com>
2020-03-05 14:21:59 +0800

    add regular expression matching

---
+++ 10_RegularExpressionMatching/regularExpressionMatching.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+/*
+backtracking
+ */
+func isMatch(s string, p string) bool {
+	if len(p) == 0 {
+		return len(s) == 0
+	}
+
+	firstMath := len(s) != 0 && ( s[0] == p[0] || p[0] == '.')
+
+	if len(p)>=2 && p[1] == '*' {
+		return isMatch(s, p[2:]) || (firstMath && isMatch(s[1:], p))
+	} else {
+		return firstMath && isMatch(s[1:], p[1:])
+	}
+}
+
+func main() {
+	fmt.Println(isMatch("aa", "a*"))
+}
