ddcd54a4e40fb27690f3c0b4354bae9a769483c6
Macrov Gao <backfire.ghw@gmail.com>
2020-03-13 17:13:21 +0800

    update

--- 20_validparentheses/valid_parentheses.go
+++ 20_validparentheses/valid_parentheses.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var m = map[rune]rune{
+var m = map[uint8]uint8{
 	')'	:	'(',
 	']'	:	'[',
 	'}'	:	'{',
@@ -12,13 +12,13 @@ func isValid(s string) bool {
 	if len(s) % 2 != 0 {
 		return false
 	}
-	var stack []rune
+	var stack []uint8
 	for i:=0;i<len(s);i++ {
 		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
-			stack = append(stack, rune(s[i]))
+			stack = append(stack, s[i])
 		} else if len(stack) > 0 {
 			last := stack[len(stack) -1]
-			if last != m[rune(s[i])] {
+			if last != m[s[i]] {
 				break
 			} else {
 				stack = stack[:len(stack)-1]
