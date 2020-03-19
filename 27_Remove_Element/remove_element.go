52c582bde7f72a85e1b0a0f83c2e98546c8d46aa
Macrov Gao <backfire.ghw@gmail.com>
2020-03-19 11:39:47 +0800

    add remove element

---
+++ 27_Remove_Element/remove_element.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+	for i:=0;i<len(nums); {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+	return len(nums)
+}
+
+func main() {
+	fmt.Println(removeElement([]int{0,1,2,2,3,0,4,2}, 2))
+}
